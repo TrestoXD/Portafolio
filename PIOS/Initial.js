c647020690b7b26ea22d87a4082d309979f79309
TrestoXD <vitogamer08@gmail.com>
2026-01-08 16:54:59 +0100

    Startmenu 0.1

--- PIOS/Initial.js
+++ PIOS/Initial.js
@@ -1,5 +1,25 @@
-console.log("Initiating Countdown...");
-setTimeout(StartOS, 4000);
+let debugmode = false;
+let timer = 4;
+
+if(debugmode == false){
+    console.log("Initiating Countdown...");
+    ChangeTimer();
+    setTimeout(StartOS, 5000);
+}
+
+function ChangeTimer(){
+    setTimeout(() => document.getElementById("countdown-text").innerText = 3 + "s", 1000);
+
+    setTimeout(() => document.getElementById("countdown-text").innerText = 3 + "s", 2000);
+
+    setTimeout(() => document.getElementById("countdown-text").innerText = 2 + "s", 3000);
+
+    setTimeout(() => document.getElementById("countdown-text").innerText = 1 + "s", 4000);
+}
+
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 function StartOS(){
     console.log("OS initiated")
