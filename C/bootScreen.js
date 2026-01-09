91413554ccbe713761beadc26788356dc0dfc9d2
TrestoXD <vitogamer08@gmail.com>
2026-01-09 10:10:59 +0100

    BS 0.2
    Imagen de fondo y temorizador añadidos

---
+++ C/bootScreen.js
@@ -0,0 +1,13 @@
+let debugmode = false;
+let timer = Math.random(4,5)
+console.log(timer)
+
+if(debugmode == false){
+    console.log("Initiating Countdown...");
+    setTimeout(StartOS, (timer) * 10000);
+}
+
+function StartOS(){
+    console.log("OS initiated")
+    window.open("/C/Desktop.html", "_self")
+}
