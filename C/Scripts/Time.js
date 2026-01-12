68924f074dff06be905716742db3534c4801aace
TrestoXD <vitogamer08@gmail.com>
2026-01-12 19:20:30 +0100

    Time

---
+++ C/Scripts/Time.js
@@ -0,0 +1,5 @@
+let currentHour = new Date().getHours().toLocaleString();
+let currentMin = new Date().getMinutes().toLocaleString();
+let currentSec = new Date().getSeconds().toLocaleString();
+
+console.log(currentHour , currentMin, currentSec);
