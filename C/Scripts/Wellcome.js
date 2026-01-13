a34c606fed2a1f31a132144b89ea4dd149fb8b16
TrestoXD <vitogamer08@gmail.com>
2026-01-13 18:06:23 +0100

    Welcome 0.1

--- C/Scripts/Wellcome.js
+++ C/Scripts/Wellcome.js
@@ -4,6 +4,64 @@ window.onload = function(){
     if(debug == true || !localStorage.getItem('visited')){
         console.log("first Time")
         
+        WellcomeExe();
         localStorage.setItem('visited', true);
     }
+}
+
+
+function WellcomeExe(){
+
+    const mainDiv = document.createElement("div");
+
+    mainDiv.classList.add("Window")
+    mainDiv.style.width = '500px';
+    mainDiv.style.height= '250px';
+    mainDiv.innerHTML = `
+    <!-- THE MAIN TOP BAR -->
+        <div class="WTop-Bar"> 
+            <div> <p>Bienvenido</p> </div> 
+            <div> 
+                <!--<button > <img src="./Styles/icons/minimize.svg" style="transform: translate(-50%, 50%);"> </button> 
+                <button> <img src="./Styles/icons/maximize.svg"> </button> -->
+                <button style="margin-left:2px"> <img src="./Styles/icons/close.svg"> </button>
+            </div>   
+        </div>
+
+        <div class="WContent" style="padding-left:20px; padding-right:20px; margin-top: 25px;"> 
+            <h1>Bienvenido a mi Portafolio</h1> 
+            <div class="WNotes" style="margin-top: 25px; margin-right:40px"> 
+                <div><img></div> 
+                <div> <b>Sabías que...</b> <p id="welcomefact">Texto...</p> </div>      
+            </div> 
+            <div>    </div>    
+        </div>
+    `
+
+    const currentDiv = document.getElementById("div1");
+    document.body.insertBefore(mainDiv, currentDiv);
+
+    RandomFact();
+}
+
+function RandomFact(){
+    let generatedFact = "";
+    let generatedNum = 0;
+
+    let Facts = ['El fuego quema','Un rayo cósmico NO fue el causante del glitch en un speedrun de Mario 64','Las zapatillas se ponen en los pies','Cuando construyeron las pirámides de Egipto aún existían los mamuts'];
+
+    function getRandomInt(min,max){
+        const mincel = Math.ceil(min);
+        const maxflor = Math.floor(max);
+        return Math.floor(Math.random() * (maxflor - mincel) + mincel);
+    }
+
+    generatedNum = getRandomInt(0,Facts.length);
+
+    generatedFact = Facts[generatedNum];
+
+    console.log(generatedNum)
+    console.log(generatedFact)
+
+    document.getElementById("welcomefact").innerText = generatedFact;
 }
