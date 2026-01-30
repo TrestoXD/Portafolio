e891fe26544ee0b2494ff936c2c83f3ebf984cae
TrestoXD <vitogamer08@gmail.com>
2026-01-30 17:48:46 +0100

    Sub menus º\(-_-)/º
    Mas o menos funcionan los sub menus de el Start Menu

--- C/Scripts/mainmenu.js
+++ C/Scripts/mainmenu.js
@@ -7,8 +7,8 @@ function mainmenuExe(){
                 <img src="./Styles/icons/sidebar95.png" id="minmenus" style="height:200px; ">
             </div>
             <div style="display:flex; flex-direction:column; width:100%;">
-                <button> <img src="./Styles/icons/directory_program_group.ico"> <p>Programas</p> <img class="arrow" src="./Styles/icons/Arrow.ico"> </button>
-                <button> <img src="./Styles/icons/documents_normal.ico"> <p>Documentos</p> <img class="arrow" src="./Styles/icons/Arrow.ico"> </button>
+                <button onmouseenter="MainmenusecExe('200px','166px',true,true, '${Programs_Menu}')"> <img src="./Styles/icons/directory_program_group.ico"> <p>Programas</p> <img class="arrow" src="./Styles/icons/Arrow.ico"> </button>
+                <button onmouseenter="MainmenusecExe('200px','166px',true,true, '${Documentos_Menu}')"> <img src="./Styles/icons/documents_normal.ico"> <p>Documentos</p> <img class="arrow" src="./Styles/icons/Arrow.ico"> </button>
                 <button> <img src="./Styles/icons/settings_gear.ico" style="scale:1.5;"> <p>Configuración</p> <img class="arrow" src="./Styles/icons/Arrow.ico"> </button>
                 <button> <img src="./Styles/icons/search_file.ico"> <p>Encuentrame</p> <img class="arrow" src="./Styles/icons/Arrow.ico"> </button>
                 <button> <img src="./Styles/icons/help_book_big.ico"> <p>Ayuda</p></button>
@@ -19,3 +19,8 @@ function mainmenuExe(){
         </div>
         `);
 }
+
+let Programs_Menu = `<div> <button>Accessories</button> <button>Online Services</button> <button>StartUp</button> <button>Internet Explorer</button> <button>CMD</button> <button>Mail</button> <button>Windows Explorer</button> </div>`
+let Documentos_Menu = `<div> <button>Readme.txt</button> </div>`
+let Settings_Menu = `<div> <button>Control Panel</button> <button>Taskbar & Start Menu...</button> </div>`
+let Find_Menu = `<div> <button>Files or Folders...</button> <button>On The XD Network...</button> </div>`
---
+++ C/Scripts/winmenu2.js
@@ -0,0 +1,23 @@
+function MainmenusecExe(X, Y, Bottom, autohide, content){
+    let menu = document.createElement("div")
+    menu.id = "secmenu"
+
+    if(Bottom == true){
+        menu.style = `bottom:${Y}; left:${X}`
+    }else{
+        menu.style = `top:${Y}; left:${X}`
+    }
+    
+
+    if(menu.parentNode){
+        menu.parentNode.removeChild(menu);
+    }
+
+    if(autohide == true){
+        menu.onmouseleave = () => secmenu.outerHTML = '', onmenu = false;    
+    }else{
+    }
+
+    menu.innerHTML = content;
+    document.body.appendChild(menu)
+}
