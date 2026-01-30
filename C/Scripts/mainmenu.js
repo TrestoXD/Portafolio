b69e1ead79b225b18bfa1b66d6ef713f9b3b3314
TrestoXD <vitogamer08@gmail.com>
2026-01-30 12:48:20 +0100

    Main Menu Buttons added!

--- C/Scripts/mainmenu.js
+++ C/Scripts/mainmenu.js
@@ -2,6 +2,20 @@
 function mainmenuExe(){
     console.log("opening menu...")
     MainmenuExe('0px','40px', true,false,`
-        <img src="./Styles/icons/sidebar95.png" id="minmenus"></img>
+        <div style="display:flex">
+            <div style="height:300px; background-color:gray; display:flex; align-items: flex-end;">
+                <img src="./Styles/icons/sidebar95.png" id="minmenus" style="height:200px; ">
+            </div>
+            <div style="display:flex; flex-direction:column; width:100%;">
+                <button> <img src="./Styles/icons/directory_program_group.ico"> <p>Programas</p> <img class="arrow" src="./Styles/icons/Arrow.ico"> </button>
+                <button> <img src="./Styles/icons/documents_normal.ico"> <p>Documentos</p> <img class="arrow" src="./Styles/icons/Arrow.ico"> </button>
+                <button> <img src="./Styles/icons/settings_gear.ico" style="scale:1.5;"> <p>Configuración</p> <img class="arrow" src="./Styles/icons/Arrow.ico"> </button>
+                <button> <img src="./Styles/icons/search_file.ico"> <p>Encuentrame</p> <img class="arrow" src="./Styles/icons/Arrow.ico"> </button>
+                <button> <img src="./Styles/icons/help_book_big.ico"> <p>Ayuda</p></button>
+                <button> <img src="./Styles/icons/application_hourglass.ico"> <p>Ejecuta</p></button>
+                <hr>
+                <button style="bottom:0; "> <img src="./Styles/icons/shut_down_normal.ico" style="height: 50px;"> <p>Apagar...</p></button>
+            </div>
+        </div>
         `);
 }
