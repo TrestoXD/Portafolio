3dbb95ff31ba97e60df977a7776a461f5b4204b9
TrestoXD <vitogamer08@gmail.com>
2026-02-03 12:12:47 +0100

    Readme working!

--- C/Scripts/Softwares/ReadMeExe.js
+++ C/Scripts/Softwares/ReadMeExe.js
@@ -1,19 +1,45 @@
+let height = 400
 
 function ReadmeEXE(){
-    WindowExe('ReadMe.txt', 400, 100, null, true, true, `
+    WindowExe('ReadMe.txt', 400, height, null, true, true, `
         <div class="notepad">
             <button>File</button>
             <button>Edit</button>
             <button>Search</button>
             <button>Help</button>
-            <div>
+            <div style="height:calc(${height}px - 49px)">
                 <pre>  
-    ------------------------------------------------------------
+    ------------------------------------------------------------------------------------------
             Portfolio 95 Leeme para la información General         
 
                             Febrero 2026
-    ------------------------------------------------------------
-                </pre>
+    ------------------------------------------------------------------------------------------
+    
+    This document provides complementary information to supplement the Portfolio
+
+    --------------------------
+    How to Use this document
+    --------------------------
+
+    TEXTO
+    TEXTO
+    TEXTO
+    TEXTO
+    TEXTO
+    TEXTO
+    TEXTO
+    TEXTO
+    TEXTO
+
+    TEXTOTEXTO
+    TEXTOTEXTO
+    TEXTO
+    TEXTO
+    TEXTO
+
+    TEXTO
+    TEXTO
+    </pre>
             </div>
         </div>
         `);
