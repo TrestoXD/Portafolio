d5a71f9080f9b39d71a177cf26fe8588457a6cc2
TrestoXD <vitogamer08@gmail.com>
2026-02-06 13:01:29 +0100

    Working on CMD!

---
+++ C/Scripts/Softwares/cmdexe.js
@@ -0,0 +1,20 @@
+
+
+function CmdEXE(){
+    WindowExe('COMMAND PROMPT', 400, 200, null, true, true, `
+    <div class="Command-prompt"> 
+    
+    <p>Vito Minaya González CC0 1.0 Universal</p>
+    
+    <span> C : \ Verse > </span><input type="text" id="inputcommand"> 
+    
+    </div>
+        `)
+    document.getElementById('inputcommand').addEventListener('keypress',CMDINPUT(e));
+}
+
+function CMDINPUT(e){
+    if(e.keycode==13){
+        console.log("enter pressed")
+    } 
+}
