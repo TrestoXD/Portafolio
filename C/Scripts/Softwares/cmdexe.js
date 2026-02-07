9ea477828aaa2956c44185ee55c76c8e3ee10aa9
TrestoXD <vitogamer08@gmail.com>
2026-02-07 09:34:37 +0100

    B26.0.1
    Added functional CMD

--- C/Scripts/Softwares/cmdexe.js
+++ C/Scripts/Softwares/cmdexe.js
@@ -1,8 +1,8 @@
 
 
 function CmdEXE(){
-    WindowExe('COMMAND PROMPT', 400, 200, null, true, true, `
-    <div class="Command-prompt"> 
+    WindowExe('COMMAND PROMPT', 400, 200, 'ms_dos-1', true, true, `
+    <div class="Command-prompt" id="Commandprompt"> 
     
     <p>Vito Minaya González CC0 1.0 Universal</p>
     
@@ -14,9 +14,27 @@ function CmdEXE(){
 }
 
 function CMDINPUT(e){
+    let CMDOUTPUT = document.getElementById("Commandprompt");
+
     if(e.keycode === 13 || e.key === 'Enter'){
-        console.log("enter pressed");
+
+        let result = document.createElement("div")
+
+        console.log(document.getElementById('inputcommand').value);
 
         //Comandos!
+        if(document.getElementById('inputcommand').value == ""){
+            result.innerHTML = `<span> C : \ Verse > </span><input type="text" id="inputcommand">`
+            document.getElementById('inputcommand').outerHTML = '';
+        }else{
+            result.innerHTML = `
+            <p> ${document.getElementById('inputcommand').value} is not recognized has an internal or external command. </p>
+            <span> C : \ Verse > </span><input type="text" id="inputcommand">`
+
+            document.getElementById('inputcommand').outerHTML = '';
+        }
+
+        CMDOUTPUT.appendChild(result)
+        document.getElementById('inputcommand').addEventListener('keypress',CMDINPUT);
     } 
 }
