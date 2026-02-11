bbda637513ae203899418b418bbc32338387e6d9
TrestoXD <vitogamer08@gmail.com>
2026-02-11 19:23:32 +0100

    B26.0.7
    PIOS IN PROGRESS

--- PIOS/Initial.js
+++ PIOS/Initial.js
@@ -1,19 +1,24 @@
-let debugmode = true;
+let debugmode = false;
+const time = 10;
+
+document.body.addEventListener('keypress', BootPress);
+const sel = document.getElementById("select")
+
+sel.addEventListener('change',function(e){
+    let value = sel.options[sel.options.selectedIndex].value;
+    console.log("The value is: "+ value)    
+});
 
 if(debugmode == false){
     console.log("Initiating Countdown...");
     ChangeTimer();
-    setTimeout(StartBoot, 5000);
+    setTimeout(StartBoot, (10 * 1000));
 }
 
 function ChangeTimer(){
-    setTimeout(() => document.getElementById("countdown-text").innerText = 3 + "s", 1000);
-
-    setTimeout(() => document.getElementById("countdown-text").innerText = 3 + "s", 2000);
-
-    setTimeout(() => document.getElementById("countdown-text").innerText = 2 + "s", 3000);
-
-    setTimeout(() => document.getElementById("countdown-text").innerText = 1 + "s", 4000);
+    for (let i = 1; i < time; i++) {
+        setTimeout(() => document.getElementById("countdown-text").innerText = i + "s", 1000);   
+    }
 }
 
 function delay(ms) {
@@ -23,4 +28,16 @@ function delay(ms) {
 function StartBoot(){
     console.log("BootScreen initiated")
     window.open("./C/Startmenu.html", "_self")
+}
+
+function BootPress(e){
+    if(e.keycode === 13 || e.key === 'Enter'){
+        let value = sel.options[sel.options.selectedIndex].value;
+        if(value == 1){
+            console.log("BootScreen initiated")
+            window.open("./C/Startmenu.html", "_self")            
+        }else{
+            alert("This OS is not prepared...")
+        }
+    }
 }
