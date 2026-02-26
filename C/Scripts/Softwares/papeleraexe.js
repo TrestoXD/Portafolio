5c3c2caf013791c56e519c6b528055bb5db3b50a
TrestoXD <vitogamer08@gmail.com>
2026-02-26 19:22:29 +0100

    B26.1.2
    Haciendo la papelera

--- C/Scripts/Softwares/papeleraexe.js
+++ C/Scripts/Softwares/papeleraexe.js
@@ -1,7 +1,23 @@
 function PapeleraExe(){
     WindowExe('Papelera de reciclaje', 400, 200, 'emptyreciclebin', true, true, `
-    <div>
-        
+    <div class="papelera">
+        <button>File</button>
+        <button>Edit</button>
+        <button>View</button>
+        <button>Help</button>
+        <div class="archivos-papelera">
+            <div class="archivos-papelera-tipos">
+                <p>Name</p>
+                <p>Localización Original</p>
+                <p>Date Deleted</p>
+            </div>
+            <div class="archivos-papelera-archivos">
+            </div>
+        </div>
+        <div style="display:flex; width:100% ">
+            <div class="papelerainfo">0 objeto(s)</div>
+            <div class="papelerainfo">0 bytes</div>
+        </div>
     </div>
         `);
 }
