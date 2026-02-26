ab1504d77d690078824a381b27eb9ab3b886390f
TrestoXD <vitogamer08@gmail.com>
2026-02-26 12:34:48 +0100

    B26.0.14

--- C/Scripts/mainmenu.js
+++ C/Scripts/mainmenu.js
@@ -10,8 +10,8 @@ function mainmenuExe(){
                 <button onmouseenter="MainmenusecExe('200px','166px',true,true, '${Programs_Menu}')"> <img src=./Styles/appicons/directory_program_group.ico> <p>Programas</p> <img class=arrow src=./Styles/icons/Arrow.ico> </button>
                 <button onmouseenter="MainmenusecExe('200px','272px',true,true, '${Documentos_Menu}')"> <img src=./Styles/appicons/documents_normal.ico> <p>Documentos</p> <img class=arrow src=./Styles/icons/Arrow.ico> </button>
                 <button onmouseenter="MainmenusecExe('200px','206px',true,true, '${Settings_Menu}')"> <img src=./Styles/appicons/settings_gear.ico style=scale:1.5;> <p>Configuración</p> <img class=arrow src=./Styles/icons/Arrow.ico> </button>
-                <button onmouseenter="MainmenusecExe('200px','166px',true,true, '${Find_Menu}')"> <img src=./Styles/appicons/search_file.ico> <p>Buscar</p> <img class="arrow" src=./Styles/icons/Arrow.ico> </button>
-                <button onmouseenter="Delete_MainmenusecExe()"> <img src=./Styles/appicons/help_book_big.ico> <p>Ayuda</p></button>
+                <button onmouseenter="MainmenusecExe('200px','166px',true,true, '${Find_Menu}')"> <img src=./Styles/appicons/searchfile.ico> <p>Buscar</p> <img class="arrow" src=./Styles/icons/Arrow.ico> </button>
+                <button onmouseenter="Delete_MainmenusecExe()"> <img src=./Styles/appicons/helpbookbig.ico> <p>Ayuda</p></button>
                 <button onmouseenter="Delete_MainmenusecExe()"> <img src=./Styles/appicons/applicationhourglass.ico> <p>Ejecuta</p></button>
                 <hr>
                 <button onmouseenter="Delete_MainmenusecExe()" style=bottom:0;> <img src=./Styles/appicons/shutdownnormal2.png style=height: 55px; margin:-6px> <p>Apagar...</p></button>
